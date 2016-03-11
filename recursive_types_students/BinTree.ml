1c0e1865a4d470f736e4023a3696ad035da1559e
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-11 10:51:50 +0100

    Add recursive types exercises

---
+++ recursive_types_students/BinTree.ml
@@ -0,0 +1,15 @@
+open Tree
+let todo _ = failwith "Ups, there is still something not implemented."
+
+(*A binary tree that also stores information in its inner nodes.
+ * The key and the value are equal.*)
+module BinTree : Tree = struct
+        type 'a t = todo
+        let empty = todo 
+        let to_list t = todo
+        let add p l = todo
+        let rec fold f a t = todo
+        let merge a b = todo
+        let min t = todo
+        let greater_than a n = todo
+end
