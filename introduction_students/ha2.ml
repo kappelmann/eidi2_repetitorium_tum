80cdefbf3d1cf1e431b34f4ad0e5cb73e9aeb3c1
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-11 10:25:28 +0100

    Add sol. for last ex. in ha2

--- introduction_students/ha2.ml
+++ introduction_students/ha2.ml
@@ -41,6 +41,16 @@ module MyList = struct
   let odd = todo
   let only_even = todo
   let fold_left f a = todo
+
+  let mystring = ["Hallo";"du";"EIDI2";"Student"] 
+  let ergebnis = fold_left (fun a s -> a^" "^s) "" mystring
+  let ergebnis = "Hallo du EIDI2 Student"
+
+  let ergebnis = fold_right (fun s a -> a^" "^s) "" mystring
+  let ergebnis = "Student EIDI2 du Hallo"
+  let ergebnis = fold_right (fun s a -> s^" "^a) "" mystring
+  let ergebnis = "Hallo du EIDI2 Student"
+
   let fold_right f xs = todo
   let exists p = todo
   let for_all p = todo
@@ -84,7 +94,12 @@ module Db = struct
 
   open MyList
   (* find a student by name *)
-  let find_student = todo
+  let find_student name = let rec help l = match l with [] -> None
+        | x::xs -> if x.name=name then Some x else help xs in
+        help students
+
+  let find_student name = MyList.filter (fun x -> x.name=name) students |> MyList.head
+
   (* all averages are of type float option *)
   (* calculate the average age of students that are in a given semester or above *)
   let avg_age = todo
@@ -95,5 +110,10 @@ module Db = struct
   (* calculate the grade average of a course (only passed, i.e. grade <= 4.0) *)
   let avg_grade_course_passed = todo
   (* calculate the grade average of a course for students in a given semester *)
-  let avg_grade_course_semester name = todo
+  let avg_grade_course_semester name t = 
+    let s = filter (fun s -> s.semester=t) students in
+    let g = filter (fun c -> c.course=name) grades in
+    let g = filter (fun c -> exists (fun s -> c.student=s.sname) s) g in
+    let g = map (fun g -> g.grade) g in
+    avgf g
 end
