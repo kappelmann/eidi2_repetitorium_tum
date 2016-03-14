9fbc189765e71c3b67d5609d94a73df4d1fed78f
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-14 20:31:55 +0100

    Add wrap exercise

---
+++ threads_students/WrapSelect.mli
@@ -0,0 +1,24 @@
+open Event
+
+(* Returns the value of the event that responds faster*)
+val ch_faster : 'a event -> 'a event -> 'a
+
+(* Returns the value of the event that responds later*)
+val ch_slower : 'a event -> 'a event -> 'a
+
+(* Returns the value of both events. 
+ * The result of the first event should be stored in the first
+ * value of the result tuple, the result of the second event
+ * in the second value of the result tuple.*)
+val ch_both : 'a event -> 'a event -> 'a * 'a
+
+type w = Send of string
+type r = Fetch | Receive of string
+
+(* Starts a looping thread for a mailbox which saves its messages in a FIFO-queue.
+ * Returns two channels which accept Send or Fetch operations respectivly.
+ * If a Send call is received, the sent message will be placed in front
+ * of the FIFO-queue. If a fetch call is received, the first message
+ * of the FIFO-queue will be sent back to the channel with a Receive message.
+ * If there is no message in the queue, "empty" will be used as a message.*)
+val start_box : unit -> (w channel * r channel)
