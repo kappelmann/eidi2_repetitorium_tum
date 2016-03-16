493c310585f134e2de8f6ad4f694c5eae8e9812e
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-16 17:48:31 +0100

    Fix solution for endterm 16

--- math/SparselyPopulatedVector.ml
+++ math/SparselyPopulatedVector.ml
@@ -12,7 +12,9 @@ let rec set i v = function [] -> if v<>0 then [(i,v)] else empty
         | (i',v')::xs -> if i'=i then 
                  if v<>0 then (i,v)::xs
                  else xs
-                else if i<i' then (i,v)::(i',v')::xs
+                else if i<i' then 
+                        if v<>0 then (i,v)::(i',v')::xs
+                        else (i',v')::xs
                 else (i',v')::set i v xs
 
 let rec mul s v = if s=0 then empty else match v with
@@ -28,7 +30,7 @@ let rec add_sb_vektor v w = match (v,w) with
                         if(r=0) then add_sb_vektor vt wt
                         else  (vi,r)::add_sb_vektor vt wt
 
-let rec mul_sb_vektor v w= match (v,w) with
+let rec mul_sb_vektor v w = match (v,w) with
         | (_,[]) -> 0
         | ([],_) -> 0
         | ((vi,vv)::vt, (wi,wv)::wt) -> if(vi<wi) then mul_sb_vektor vt w
