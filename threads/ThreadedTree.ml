a446da8c412b67011212d282cf24a48b975579a0
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-17 10:27:51 +0100

    Add aufgabensammlung

--- threads/ThreadedTree.ml
+++ threads/ThreadedTree.ml
@@ -4,7 +4,7 @@ let rec min t =
         let open Event in let open Thread in
         let help c t = sync(send c (min t)) in
         match t with Leaf v -> v
-        |Node (l,r) -> let (cl,cr) = new_channel() in
+        | Node (l,r) -> let (cl,cr) = (new_channel(),new_channel()) in
                         let _ = Thread.create (help cl) l in
                         let _ = Thread.create (help cr) r in
                         let lv = sync(receive cl) in
