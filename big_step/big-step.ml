2454aea464ed8d21a8c21e7ea75921a491d1c5dd
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-17 20:37:17 +0100

    Change random method

--- big_step/big-step.ml
+++ big_step/big-step.ml
@@ -3,10 +3,9 @@ let rec app l1 l2 = match l1 with [] -> l2
 (*Zeige: app l1 l2 terminiert für alle l1, l2*)
 
 (* Zeige: random b l terminiert für alle
- * boolschen Werte b und alle Listen l
+ * boolschen Werte b und alle Listen l mit dem Wert 1
  * Machen wir eventuell noch später im Rep.*)
 let rec random b l = match b with
-        | true -> match l with [] -> 0 
-                | x::xs -> random (not b) xs; ;
+        | true -> random (not b) l
         | false -> match l with [] -> 1
                 | x::xs -> random (not b) xs
