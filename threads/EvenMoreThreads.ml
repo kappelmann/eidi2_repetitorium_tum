c608f1a5d16aa97232ea21cd4b6455d8c0540e92
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-17 15:31:48 +0100

    Add solution for even more threads

--- threads/EvenMoreThreads.ml
+++ threads/EvenMoreThreads.ml
@@ -1,8 +1,7 @@
 let todo = failwith "Todo not implemented"
 
-
-open Event
 open Thread
+open Event
 
 let rec map f = function [] -> []
         | x::xs -> f x::map f xs
@@ -21,15 +20,13 @@ let select_last el = let l = length el in
                 if i=l then res else help (i+1)
         in help 1
 
-let zcount = todo
+let zcount (Zug(i',wl)) = match wl with 
+        | End -> i'
+        | Waggon(i,wl) -> i+zcount (Zug (i',wl))
 
-let avg_tzcount = todo
+let rec sum = function [] -> 0
+        | x::xs -> x+sum xs
 
-
-
-
-
-
-
-
-
+let avg_tzcount l = let zl = length l in
+        let el = tmap zcount l in
+        (float_of_int(sum el)) /. ((float_of_int)zl)
