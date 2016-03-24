1fd2b95a0aafbae7d2b8f825fb8a3eb69da6a6e1
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-24 14:51:32 +0100

    Fix test comment

--- threads/ThreadedTreeTest.ml
+++ threads/ThreadedTreeTest.ml
@@ -7,4 +7,4 @@ let _ = if e<>m then failwith "Your tree delivered the wrong result for a single
 let t = Node( Node( Node( Leaf (-10), Leaf 10), Node ( Leaf 3, Leaf (-11))), Leaf 0)
 let m = min t
 let e = (-11)
-let _ = if e=m then print_string "Your threaded tree works like a charm! +10p" else failwith "Your tree delivered the wrong result for a single leaf tree."
+let _ = if e=m then print_string "Your threaded tree works like a charm! +10p" else failwith "Your tree delivered the wrong result for a large tree."
