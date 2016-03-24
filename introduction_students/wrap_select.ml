c8e0c7ec817ed9c65d4f8566399d86e5e0b82460
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-24 15:25:23 +0100

    Fix typo

--- introduction_students/wrap_select.ml
+++ introduction_students/wrap_select.ml
@@ -3,7 +3,7 @@
 let faster = select [e1;e2]
 
 (*Wrap packt um das übergebene Event im ersten Parameter ein neues Event, indem es
- * auf das Ergebnisd es ersten Events die übergebene Funktion anwendet
+ * auf das Ergebnis es ersten Events die übergebene Funktion anwendet
  * val wrap : 'a event -> ('a -> 'b) -> 'b event *)
 
 (*Der schnellere gewinnt*)
@@ -22,7 +22,7 @@ let s = choose [
 (*So ist select im Events module definiert*)
 let select y = sync(choose y)
 
-(*Beide Ergebnisse werden empfangen*)
+(*Beide Ergebnisse werden empfangen. Zuerst vom schnelleren, dann vom langsamen*)
 let s = select [ 
            wrap(receive c_odd) (fun a -> (a, sync(receive c_even)));
            wrap(receive c_even) (fun a -> (sync(receive c_odd), a))
