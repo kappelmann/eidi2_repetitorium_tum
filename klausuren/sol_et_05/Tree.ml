551b2c80d54b3eb9d2ac34add281d24100dc32ab
Kevin Kappelmann <kevin@kappelmann.me>
2016-03-27 09:38:08 +0200

    Add official solution for et05

--- klausuren/sol_et_05/Tree.ml
+++ klausuren/sol_et_05/Tree.ml
@@ -2,10 +2,10 @@ type 'a tree = Tree of('a * 'a tree list)
 let rec size = function Tree (_,[]) -> 1
         | Tree (v, x::xs) -> (size x) + (size (Tree(v,xs)))
 
-let rec map f t = 
+let rec map f Tree(v,l) = 
         let rec sub = function [] -> []
         | x::xs -> map f x::sub xs in
-        match t with Tree(v,l) -> Tree(f v, sub l)
+        Tree(f v, sub l)
 
 let rec to_list t = 
         let rec sub l a = match l with [] -> a
