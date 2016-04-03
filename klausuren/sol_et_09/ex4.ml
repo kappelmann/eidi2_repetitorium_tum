819dd569414607350ec467e35a28abbd5aa4e799
Kevin Kappelmann <kappelmann@users.noreply.github.com>
2016-04-03 17:57:13 +0200

    Update ex4.ml

--- klausuren/sol_et_09/ex4.ml
+++ klausuren/sol_et_09/ex4.ml
@@ -4,7 +4,7 @@ let cycle = function [] -> []
 let ncycle n l = let rec doit i l = if i=n then l else doit (i+1) (cycle l)
         in doit 0 l
 
-let rec bubble = function f::s::xs -> if f<s then f::bubble x::xs else s::bubble f::xs
+let rec bubble = function f::s::xs -> if f<s then f::bubble (s::xs) else s::bubble (f::xs)
         | x -> x
 
 let rec sort l = let rec doit i n l = if i=n then l
