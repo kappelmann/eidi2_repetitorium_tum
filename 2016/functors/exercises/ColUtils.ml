4166e215c5cda528728c794ea89ef12e84095c0a
Kevin Kappelmann <kevin@kappelmann.me>
2017-03-16 21:45:05 +0100

    update todo ()

--- 2016/functors/exercises/ColUtils.ml
+++ 2016/functors/exercises/ColUtils.ml
@@ -3,11 +3,11 @@ let todo _ = failwith "Ups, you forgot to implement something."
 open Collection
 
 module ColUtils (C: Collection) = struct
-        let filter p a = todo
-        let for_all f a = todo
-        let length a = todo
-        let exists p a = todo
-        let merge a b = todo
-        let tuple a b = todo
-        let every_nth a n = todo
+        let filter p a = todo ()
+        let for_all f a = todo ()
+        let length a = todo ()
+        let exists p a = todo ()
+        let merge a b = todo ()
+        let tuple a b = todo ()
+        let every_nth a n = todo ()
 end
--- 2016/functors/exercises/EnhancedMap.ml
+++ 2016/functors/exercises/EnhancedMap.ml
@@ -1,19 +1,19 @@
-let todo = failwith "Todo implement this map method"
+let todo _ = failwith "Todo implement this map method"
 
 open SimpleMap
 
 module EnhancedMap (S: SimpleMap) = struct
         include S
-        let is_greater = todo
-        let is_smaller = todo
-        let is_equal = todo
-        let map = todo
-        let map_val = todo
-        let map_key = todo
-        let exists = todo
-        let exists_key = todo
-        let fold f a m = todo
-        let remove p m = todo
-        let remove_key = todo
-        let count_dupl = todo
+        let is_greater = todo ()
+        let is_smaller = todo ()
+        let is_equal = todo ()
+        let map = todo ()
+        let map_val = todo ()
+        let map_key = todo ()
+        let exists = todo ()
+        let exists_key = todo ()
+        let fold f a m = todo ()
+        let remove p m = todo ()
+        let remove_key = todo ()
+        let count_dupl = todo ()
 end
