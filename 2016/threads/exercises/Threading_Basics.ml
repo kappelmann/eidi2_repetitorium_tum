ecf843f215741ecb17c09a53d92a2ea5ad240390
Kevin Kappelmann <kevin@kappelmann.me>
2017-03-27 13:20:52 +0200

    Fix todo

--- 2016/threads/exercises/Threading_Basics.ml
+++ 2016/threads/exercises/Threading_Basics.ml
@@ -1,14 +1,14 @@
-let todo = failwith "Todo implement"
+let todo _ = failwith "Todo implement"
 
 (* Aufgabe 1 *)
-let threaded_filter p l c = todo
+let threaded_filter p l c = todo ()
 
 (* Aufgabe 2 *)
-type 'a t = todo
-let create = todo
-let get = todo
+type 'a t = todo()
+let create = todo ()
+let get = todo()
 
 (* Aufgabe 3 *)
-type 'a tree = todo
+type 'a tree = todo()
 
-let min = todo
+let min = todo()
